b914ff11524b0a69f0a9f6e7c6cf590f938c3ab7
TangRufus <tangrufus@gmail.com>
2023-08-30 03:22:03 +0100

    Add `wp` package

---
+++ wp/entity.go
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023 Typist Tech Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package wp
+
+import (
+	"errors"
+	"github.com/typisttech/wpsecadvi/composer/version"
+)
+
+type kind byte
+
+const (
+	core kind = iota
+	plugin
+	theme
+)
+
+var (
+	ErrEmptyEntitySlug = errors.New("empty entity string")
+)
+
+type Entity struct {
+	kind          kind
+	slug          string
+	versionRanges []version.Range
+}
+
+func NewCoreEntity() *Entity {
+	return &Entity{kind: core, slug: "wordpress-core"}
+}
+
+func NewPluginEntity(slug string) (*Entity, error) {
+	if slug == "" {
+		return nil, ErrEmptyEntitySlug
+	}
+	return &Entity{kind: plugin, slug: slug}, nil
+}
+
+func NewThemeEntity(slug string) (*Entity, error) {
+	if slug == "" {
+		return nil, ErrEmptyEntitySlug
+	}
+	return &Entity{kind: theme, slug: slug}, nil
+}
+
+func (e *Entity) IsCore() bool {
+	return e.kind == core
+}
+
+func (e *Entity) IsPlugin() bool {
+	return e.kind == plugin
+}
+
+func (e *Entity) IsTheme() bool {
+	return e.kind == theme
+}
+
+func (e *Entity) Slug() string {
+	return e.slug
+}
+
+func (e *Entity) VersionRanges() []version.Range {
+	return e.versionRanges
+}
+
+func (e *Entity) Or(r version.Range) {
+	e.versionRanges = append(e.versionRanges, r)
+}
